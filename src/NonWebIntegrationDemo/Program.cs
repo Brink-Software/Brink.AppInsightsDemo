55aba590eafba60aefbc01b14ee70918cfdb8432
Peter Bons <msn@2000miles.nl>
2018-12-14 11:28:38 +0100

    Added basic demo of non-web integration

---
+++ src/NonWebIntegrationDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Extensibility;
+using NonWebIntegrationDemo.Properties;
+
+namespace NonWebIntegrationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new TelemetryConfiguration
+            {
+                InstrumentationKey = Settings.Default.AppInsightsKey
+            };
+
+            new LiveStreamProvider(configuration).Enable();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
