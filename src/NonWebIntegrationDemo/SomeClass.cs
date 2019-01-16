96d0aa5e468ea5bd57a4e083258dae62752806f6
Peter Bons <github@2000miles.nl>
2019-01-16 13:56:53 +0100

    Support  nesting of diagnostics

---
+++ src/NonWebIntegrationDemo/SomeClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace NonWebIntegrationDemo
+{
+    public class SomeClass
+    {
+        private readonly TelemetryClient _telemetryClient;
+
+        public SomeClass(TelemetryClient telemetryClient)
+        {
+            _telemetryClient = telemetryClient;
+        }
+
+        public string SayHello(string to)
+        {
+            string response = null;
+
+            var operationName = $"{nameof(NonWebIntegrationDemo)}.{nameof(SomeClass)}.{nameof(SayHello)}";
+            using (_telemetryClient.StartOperation<RequestTelemetry>(operationName))
+            {
+                try
+                {
+                    var greeting = $"Hello {to}";
+                    _telemetryClient.TrackTrace($"Sending {greeting}");
+
+                    response = new SomeService(_telemetryClient).Send(greeting);
+                }
+                catch (Exception exception)
+                {
+                    _telemetryClient.TrackException(exception);
+                }
+
+                return response;
+            }
+        }
+    }
+}
---
+++ src/NonWebIntegrationDemo/SomeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace NonWebIntegrationDemo
+{
+    public class SomeService
+    {
+        private readonly TelemetryClient _telemetryClient;
+
+        public SomeService(TelemetryClient telemetryClient)
+        {
+            _telemetryClient = telemetryClient;
+        }
+
+        public string Send(string greeting)
+        {
+            var operationName = $"{nameof(NonWebIntegrationDemo)}.{nameof(SomeService)}.{nameof(Send)}";
+            using (var operation = _telemetryClient.StartOperation<DependencyTelemetry>(operationName))
+            {
+                try
+                {
+                    throw new Exception("Woopsie");
+                }
+                catch
+                {
+                    operation.Telemetry.Success = false;
+                    throw;
+                }
+            }
+        }
+    }
+}
